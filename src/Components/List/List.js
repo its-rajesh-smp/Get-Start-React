6b0c302c9da7ebc48d0bef2692297d460e373643
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-03-12 11:56:10 +0530

    Props || Looping Through Data Push

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,25 +1,11 @@
-function List() {
-
-    let date = new Date()
-
-
+function List(props) {
 
     return (
         <div>
             <div style={{ border:'1px solid black', display: "flex", justifyContent: "space-evenly", alignItems: 'center', backgroundColor: 'skyblue' }}>
-                <p>Mobile</p>
-                <p>{date.getDate()}</p>
-                <p>200$</p>
-            </div>
-            <div style={{ border:'1px solid black', display: "flex", justifyContent: "space-evenly", alignItems: 'center', backgroundColor: 'skyblue' }}>
-                <p>Petrol</p>
-                <p>{date.getDate()}</p>
-                <p>20$</p>
-            </div>
-            <div style={{ border:'1px solid black', display: "flex", justifyContent: "space-evenly", alignItems: 'center', backgroundColor: 'skyblue' }}>
-                <p>Food</p>
-                <p>{date.getDate()}</p>
-                <p>5$</p>
+                <p>{props.name}</p>
+                <p>{props.date}</p>
+                <p>{props.price}$</p>
             </div>
         </div>
     )
--- src/Components/ListContainer/ListContainer.js
+++ src/Components/ListContainer/ListContainer.js
@@ -1,11 +1,73 @@
 import ListInitial from '../ListInitial/ListInitial';
 import List from '../List/List';
 import './ListContainer.css'
-function ListContainer(){
+// ============================STORAGE START==============================
+
+
+const value = [
+
+    {
+        _id:2566,
+        name: "Mobile",
+        date: new Date().getDay(),
+        price: "250"
+    },
+
+    {
+        _id:2567,
+        name: "HeadPhone",
+        date: new Date().getDay(),
+        price: "20"
+    },
+
+    {
+        _id:2568,
+        name: "Food",
+        date: new Date().getDay(),
+        price: "5"
+    },
+
+    {
+        _id:2569,
+        name: "Shirt",
+        date: new Date().getDay(),
+        price: "10"
+    },
+
+    {
+        _id:2570,
+        name: "Mobile Cover",
+        date: new Date().getDay(),
+        price: "3"
+    },
+
+    {
+        _id:2571,
+        name: "Sim Card",
+        date: new Date().getDay(),
+        price: "4"
+    }
+
+
+]
+
+
+
+
+
+// ============================STORAGE END==============================
+
+
+
+
+
+
+function ListContainer() {
+
+    let data=value.map((item)=><List name={item.name} date={item.date} price={item.price} ></List>)
     return (
         <div className="ListContainer">
-            <ListInitial></ListInitial>
-            <List></List>
+            {data}
         </div>
 
     );
